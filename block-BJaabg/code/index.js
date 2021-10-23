8c31e5fd981c7061683c2381006c502e3b7507f4
chandresh dave <chandreshd89@gmail.com>
2021-10-23 11:27:18 +0530

    block-BJaabg assignment completed

--- block-BJaabg/code/index.js
+++ block-BJaabg/code/index.js
@@ -15,4 +15,15 @@ var bank_balance = 303.91;
 var amount = 0;
 // your code goes here
 
+while (bank_balance > amount) {
+  amount += PHONE_PRICE;
+  if (amount < SPENDING_THRESHOLD) {
+    amount += ACCESSORY_PRICE;
+  }
+}
+
+const amountAfterTax = amount + amount * TAX_RATE;
+const finalAmount = "$" + amountAfterTax.toFixed(2);
+console.log(finalAmount);
+
 // ⛑ Answer of the above will `$334.76`.
